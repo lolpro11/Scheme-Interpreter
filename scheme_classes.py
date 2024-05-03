80da9fe0b72f161f3f38989d46c1034dc98b54ff
lolpro11 <lolpro11lolx@gmail.com>
2024-05-03 16:46:43 -0700

    problem 1

--- scheme_classes.py
+++ scheme_classes.py
@@ -12,6 +12,7 @@ class SchemeError(Exception):
 class Frame:
     """An environment frame binds Scheme symbols to Scheme values."""
 
+        "*** YOUR CODE HERE ***"
     def __init__(self, parent):
         """An empty frame with parent frame PARENT (which may be None)."""
         self.bindings = {}
@@ -26,13 +27,14 @@ class Frame:
     def define(self, symbol, value):
         """Define Scheme SYMBOL to have VALUE."""
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        self.bindings.insert(symbol, value)
         # END PROBLEM 1
 
     def lookup(self, symbol):
         """Return the value bound to SYMBOL. Errors if SYMBOL is not found."""
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        if value is not None:
+            return self.bindings.get(symbol)
         # END PROBLEM 1
         raise SchemeError('unknown identifier: {0}'.format(symbol))
 
